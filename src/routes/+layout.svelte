6c685955e0c046983a754af19dd0d7c3eda5ef2e
AmazinAxel <87440159+AmazinAxel@users.noreply.github.com>
2024-02-26 08:21:57 -0800

    Sync

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,7 @@
 	import { preloadCode } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
+	import '../styles/globals.css';
 	export let data;
 
 	const transitionIn = { delay: 150, duration: 150 };
@@ -30,21 +31,6 @@
 		preloadCode(...navRoutes);
 	});
 </script>
-
-<svelte:head>
-	<link rel="stylesheet" href="/css/vars.css" />
-	<link rel="stylesheet" href="/css/root.css" />
-	<link rel="stylesheet" href="/css/fonts.css" />
-	<link rel="stylesheet" href="/css/typography.css" />
-	<link rel="stylesheet" href="/css/layout.css" />
-	<link rel="stylesheet" href="/css/components.css" />
-	<link rel="stylesheet" href="/css/header-and-footer.css" />
-	<link rel="stylesheet" href="/css/forms.css" />
-	<link rel="stylesheet" href="/css/animation.css" />
-	<link rel="stylesheet" href="/css/utilities.css" />
-	<link rel="stylesheet" href="/css/code.css" />
-	<link rel="stylesheet" href="/css/prism.css" />
-</svelte:head>
 
 <!--
 	The below markup is used on every page in the site. The <slot> is where the page's
