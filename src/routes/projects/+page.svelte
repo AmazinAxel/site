314cd5e72507337bfd03b42b71a6d8138de1eccb
AmazinAxel <87440159+AmazinAxel@users.noreply.github.com>
2024-03-21 12:08:55 -0700

    TODO: fix tailwind stuff

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
+
+<h1>Projects</h1>
+
+<h2> Projects </h2>
+<p>I created or participated a large part in the creation of:</p>
+
+
+<h2> Small Projects </h2>
+<p>These projects were created for specific purposes quickly:</p>
