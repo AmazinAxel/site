a1166284d251e1da4cc1679201040d250cc869a0
AmazinAxel <ars18451@gmail.com>
2024-05-05 14:06:25 -0700

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
 export default {
-  content: ['./src/**/*.{svelte,js}'], // Remove unused CSS files
+  //content: ['./src/**/*.{svelte,js}'], // Remove unused CSS files
   //darkmode: "media" // or true or 'media' or 'class'
 }
