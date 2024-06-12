8b4aa080cef33db1b1ef1fd60852bd277fd1ecb7
AmazinAxel <ars18451@gmail.com>
2024-06-12 12:45:01 -0700

    OMG LETS GO FRRRRR

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 export default {
-  content: ['./src/**/*.{svelte,js}'], // Remove unused CSS files
-  //darkmode: "media" // or true or 'media' or 'class'
-}
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+      extend: {}
+  },
+  plugins: []
+};
