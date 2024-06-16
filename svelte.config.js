0343ef2492d2d58079fb8f3935d99163cf6be92e
AmazinAxel <ars18451@gmail.com>
2024-06-15 18:28:59 -0700

    Fix compressing issue and speed up build process

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Ensures .svelte files are treated as components (can be imported anywhere)
 	extensions: ['.svelte'],
 
 	// Tells Sveltekit where to look for files
 	kit: {
-		adapter: adapter({ precompress: true })
+		adapter: adapter()
 	},
-	preprocess: vitePreprocess()
 };
 
 export default config;
