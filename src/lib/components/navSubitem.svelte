f93bef307170e38a862eb588e84b9fa0a6bb410c
AmazinAxel <ars18451@gmail.com>
2024-06-18 20:14:00 -0700

    test fix (stage 2)

---
+++ src/lib/components/navSubitem.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { currentPage } from '../assets/js/store';
+
+	export let href;
+
+	$: isCurrentPage = $currentPage.startsWith(href);
+
+</script>
+
+<li>
+	<a
+		{href}
+		class:active={isCurrentPage}
+		aria-current={isCurrentPage ? 'page' : false}
+	>
+		<slot/>
+	</a>
+</li>
