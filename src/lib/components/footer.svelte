0f67e1b5ce54cba8e9b6d8630ffb2734d4e10864
AmazinAxel <ars18451@gmail.com>
2024-10-15 18:17:13 -0700

    Improve footer gen date

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -4,6 +4,5 @@
 
 <footer>
 	<p>&copy;2016-{new Date().getFullYear()} AmazinAxel (Alec) • All Rights Reserved</p>
-	<p>You aren't tracked here - have a nice day</p>
-	<p style="float: right">{date}</p>
+	<p class="date">Site last generated on {date}</p>
 </footer>
--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -9,7 +9,17 @@ export const csr = true;
 export const load = async () => {
 	try {
 		return {
-			date: new Date().toLocaleString()
+			date: new Date().toLocaleString("en-US", {
+				weekday: 'long',
+				year: 'numeric',
+				month: 'long',
+				day: 'numeric',
+	
+				hour: 'numeric',
+				minute: 'numeric',
+				hour12: true,
+				timeZoneName: 'short'
+			})
 		};
 	} catch (err) {
 		error(500, err);
