f03a2084fbae28294aa018e950b3808384a553f8
AmazinAxel <ars18451@gmail.com>
2024-11-22 17:26:59 -0800

    Cleanup

--- resources/[resource]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let data;
-    const { articleContent } = data;
-    const { title } = data.meta;
-</script>
-<svelte:head> <!-- Custom title ending -->
-	<title>{ title } - AmazinAxel's Resources</title>
-</svelte:head>
-<svelte:component this={ articleContent }/>
