5286548f2c8e136a3ab8d4946ff46fbfa6c3d577
AmazinAxel <axel@amazinaxel.com>
2025-05-13 18:19:52 -0700

    fix: fix build error (attempt 1)

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: vitePreprocess(),
 	// Build error pages
 	kit: { adapter: adapter({ fallback: '404.html' }) }
 };
